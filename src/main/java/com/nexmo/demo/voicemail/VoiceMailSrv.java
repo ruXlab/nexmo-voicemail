061f18895f12a61bb3e8fa64113e98fb0a5c6036
ruX <post4ruX@gmail.com>
2018-03-04 14:09:50 +0000

    Move SMTP configuration to properties file

--- src/main/java/com/nexmo/demo/voicemail/VoiceMailSrv.java
+++ src/main/java/com/nexmo/demo/voicemail/VoiceMailSrv.java
@@ -35,21 +35,4 @@ public class VoiceMailSrv {
     ) throws IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidKeySpecException {
         return new NexmoClient(new JWTAuthMethod(applicationId, Files.readAllBytes(cert.getFile().toPath())));
     }
-
-    @Bean
-    public JavaMailSender javaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("localhost");
-        mailSender.setPort(25);
-//        mailSender.setUsername("my.gmail@gmail.com");
-//        mailSender.setPassword("password");
-
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-//        props.put("mail.smtp.auth", "true");
-//        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "false");
-
-        return mailSender;
-    }
 }
